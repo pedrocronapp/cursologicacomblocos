2a880da0c5aaf7f1df0ec2670dc0edadf16fb367
Pedro Lima <pedro.lima@cronapp.io>
2020-11-03 20:06:14 +0000

    Aula 4.2 - Calculando IMC

--- src/main/webapp/js/blockly/Matematica.blockly.js
+++ src/main/webapp/js/blockly/Matematica.blockly.js
@@ -7,7 +7,7 @@ window.blockly.js.blockly.Matematica = window.blockly.js.blockly.Matematica || {
  * Matematica
  */
 window.blockly.js.blockly.Matematica.calcular_imc = async function() {
- var item, altura, peso, resultado_imc;
+ var resultado_imc, altura, peso;
   altura = parseFloat(window.prompt('Informe sua altura'));
   peso = parseFloat(window.prompt('Informe seu peso'));
   resultado_imc = peso / (altura * altura);
