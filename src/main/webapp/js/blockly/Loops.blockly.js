eee77e2e3e4c5496eec9e51256d33235661d5c12
Pedro Lima <pedro.lima@cronapp.io>
2020-11-11 18:52:04 +0000

    Aula 7.3 - Tabuada com For Aninhado

--- src/main/webapp/js/blockly/Loops.blockly.js
+++ src/main/webapp/js/blockly/Loops.blockly.js
@@ -4,10 +4,23 @@ window.blockly.js.blockly = window.blockly.js.blockly || {};
 window.blockly.js.blockly.Loops = window.blockly.js.blockly.Loops || {};
 
 /**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.Loops.imprimir_tabuadas = async function() {
+ var item, contador, i, j, numero_tabuada, resultado_tabuada;
+  for (i = 0; i <= 10; i++) {
+    for (j = 0; j <= 10; j++) {
+      resultado_tabuada = (i * j);
+      console.log([i,'x',j,'=',resultado_tabuada].join(''));
+    }
+  }
+}
+
+/**
  * Loops
  */
 window.blockly.js.blockly.Loops.imprimir_tabuada_com_while = async function() {
- var item, contador, numero_tabuada, resultado_tabuada;
+ var item, contador, i, j, numero_tabuada, resultado_tabuada;
   numero_tabuada = parseFloat(window.prompt('Informe um número a ser calculado na tabuada'));
   contador = 0;
   while (contador < 11) {
